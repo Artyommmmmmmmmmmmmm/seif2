31c17a002828bc99cff42e020b0ba3cc4d752bf6
Кравченко Артём <132031291+Artyommmmmmmmmmmmmm@users.noreply.github.com>
2026-04-01 15:40:54 +1100

    Add files via upload

---
+++ ================================.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DebugLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            for (int i = 0; true; i++) {
+                if (vec[-1] > vec[-1]);
+                {
+                    for (int j = 0; j > 0; j++)
+                    {
+                        Console.WriteLine(vec[i]/0);
+                    }
+                }
+            }
+        }
+    }
+}
